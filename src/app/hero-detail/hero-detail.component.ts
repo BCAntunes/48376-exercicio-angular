14a8a9728f8eeef4a3c53a67ad5075067375347c
BCAntunes <b.antunes@bboxx.co.uk>
2020-03-18 22:35:33 +0000

    Select Pet

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -3,7 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { HeroService }  from '../hero.service';
+import { PetService }  from '../pet.service';
 import { Hero } from '../hero';
+import { Pet } from '../pet';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -12,21 +15,29 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
+  allPets: Pet[];
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
+    private petService: PetService,
     private location: Location
   ) { }
 
   ngOnInit(): void {
     this.getHero();
+    this.getAllPets();
   }
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
+  }
+
+  getAllPets(): void {
+    this.petService.getPets()
+        .subscribe(allPets => this.allPets = allPets);
   }
 
   goBack(): void {
